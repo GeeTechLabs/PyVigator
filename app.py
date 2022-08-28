d89b5055e4f179d2122a959e03d6ddc72a9e8b08
GeeTechLabs <geetechlabs@gmail.com>
2022-08-28 17:57:42 +0300

    Psycopg2 Addition

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from selenium.webdriver.chrome.service import Service as BraveService
 from selenium.webdriver.chrome.service import Service as BraveService
 # from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
-
+import psycopg2
 import undetected_chromedriver as uc
 
 db_values = {
