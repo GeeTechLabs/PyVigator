9de1773d6a9c083017e32a2811c13c0ef85afa6c
GeeTechLabs <geetechlabs@gmail.com>
2022-08-29 09:16:02 +0300

    JSON Parsing & Notation Fix

--- app.py
+++ app.py
@@ -65,14 +65,13 @@ def write_to_db(json_data):
         item = json_data
         custom_log("Trying Statement Execution")
         try:
-            custom_log(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14], item[15], item[16], item[17], item[18], item[19], item[20], item[21], item[22], item[23])
+            custom_log(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[14], item[15], item[16], item[17], item[19], item[20], item[21], item[22], item[23], item[24], item[25])
         except Exception as e:
             custom_log(e)
 
         try:
             custom_log('Trying To Execute')
-            custom_log(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14], item[15], item[16], item[17], item[18], item[19], item[20], item[21], item[22], item[23])
-            cursor.execute(sql_statement, (item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14], item[15], item[16], item[17], item[18], item[19], item[20], item[21], item[22], item[23]))
+            cursor.execute(sql_statement, (item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[14], item[15], item[16], item[17], item[19], item[20], item[21], item[22], item[23], item[24], item[25]))
             custom_log('Trying To Commit')
             connection.commit()
         except Exception as e:
