d85abbb5efb95de09b414a5fdd62e8d7b7b53abc
GeeTechLabs <geetechlabs@gmail.com>
2022-08-29 09:33:53 +0300

    Cursor Debug

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def write_to_db(json_data):
             cursor.execute(sql_statement, (item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[14], item[15], item[16], item[17], item[19], item[20], item[21], item[22], item[23], item[24], item[25]))
             custom_log('Trying To Commit')
             connection.commit()
-        except Exception as e:
+        except psycopg2.Error as e:
             custom_log(e)
 
     except (Exception, psycopg2.Error) as error:
