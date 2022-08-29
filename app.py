0c62283de2810d5c738ec00f9c8fe26f7899c766
GeeTechLabs <geetechlabs@gmail.com>
2022-08-29 08:42:37 +0300

    Log JSON Items

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ def write_to_db(json_data):
         '''
         item = json_data
         custom_log("Trying Statement Execution")
+        custom_log(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14], item[15], item[16], item[17], item[18], item[19], item[20], item[21], item[22], item[23])
         try:
             custom_log('Trying To Execute')
             custom_log(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14], item[15], item[16], item[17], item[18], item[19], item[20], item[21], item[22], item[23])
@@ -88,7 +89,7 @@ def write_to_db(json_data):
         cursor = connection.cursor()
 
         for item in json_data:
-            select_statement = 'select id from comics where title = \'{0}\''.format(item["title"])
+            select_statement = 'select id from comics where title = \'{0}\''.format(item[0])
 
             cursor.execute(select_statement)
 
