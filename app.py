5da8b75a56157f7012e1a9dae26c666135f83709
GeeTechLabs <geetechlabs@gmail.com>
2022-08-29 09:56:43 +0300

    Eroor Logging

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def write_to_db(json_data):
         custom_log("Trying Statement Execution")
         try:
             custom_log(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[14], item[15], item[16], item[17], item[19], item[20], item[21], item[22], item[23], item[24], item[25])
-        except Exception as e:
+        except psycopg2.Error as e:
             custom_log(e)
 
         try:
