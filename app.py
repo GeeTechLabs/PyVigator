7a0f1c65177059bf9a9d2e376fc4c51caf8ea29a
GeeTechLabs <geetechlabs@gmail.com>
2022-08-29 10:11:41 +0300

    JSON Item Debug

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def write_to_db(json_data):
         item = json_data
         custom_log("Trying Statement Execution")
         try:
-            print(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[14], item[15], item[16], item[17], item[19], item[20], item[21], item[22], item[23], item[24], item[25])
+            print(item)
         except Exception as e:
             custom_log(e)
 
