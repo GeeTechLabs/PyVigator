982c03f20c5a193b60d208e4ecad6dc1f9d642f9
GeeTechLabs <geetechlabs@gmail.com>
2022-08-29 08:25:49 +0300

    Connection Debuging

--- app.py
+++ app.py
@@ -65,7 +65,9 @@ def write_to_db(json_data):
         item = json_data
         custom_log("Trying Statement Execution")
         try:
+            custom_log('Trying To Execute')
             cursor.execute(sql_statement, (item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13], item[14], item[15], item[16], item[17], item[18], item[19], item[20], item[21], item[22], item[23]))
+            custom_log('Trying To Commit')
             connection.commit()
         except Exception as e:
             custom_log(e)
